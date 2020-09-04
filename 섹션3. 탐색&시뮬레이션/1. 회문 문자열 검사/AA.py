a0d29c8e4ac3eb909cfec2fcda85e45a4bc57923
hiiamjh <hiiamjh@gmail.com>
2020-09-04 15:27:05 +0900

    correct #1

--- 섹션3. 탐색&시뮬레이션/1. 회문 문자열 검사/AA.py
+++ 섹션3. 탐색&시뮬레이션/1. 회문 문자열 검사/AA.py
@@ -1,7 +1,19 @@
 #import sys
-#sys.stdin=open("input.txt", "r")
+#sys.stdin = open("input.txt", "rt")
 
 n = int(input())
-for i in range(n):
-    s = input()
 
+for i in range(1,n+1):
+
+    s = input()
+    s = s.upper()
+    cnt = 0
+
+    for j in range(len(s)//2):
+        if s[j]==s[-(j+1)]: cnt += 1
+
+    if cnt == len(s)//2:
+        print("#"+str(i)+" YES")
+
+    else:
+        print("#"+str(i)+" NO")
